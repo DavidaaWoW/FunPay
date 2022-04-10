c207aa1bf288ff427f80195d27a7ad0c583281a8
nixx <nixx.dj@gmail.com>
2022-04-10 14:26:42 +0300

    new server

--- clickhouse-queue/clickhouse-queue/deploy.php
+++ clickhouse-queue/clickhouse-queue/deploy.php
@@ -20,7 +20,7 @@ $ssh_options = [
  */
 $servers = [
 	'rees46' => [
-		'servers'   => ['94.130.49.248'],
+		'servers'   => ['81.163.20.236'],
 		'deploy_to' => '/home/rails/rees46_clickhouse_queue',
 		'ssh'       => $ssh_options,
 	],
