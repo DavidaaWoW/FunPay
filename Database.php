1524d424341b81d49cb8a769182d390cc7387466
DavidaaWoW <davidik.gegiya@gmail.com>
2024-01-20 01:04:50 +0300

    push to git

---
+++ Database.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace FpDbTest;
+
+use Exception;
+use mysqli;
+
+class SkipValue{}
+
+class Database implements DatabaseInterface
+{
+
+    private mysqli $mysqli;
+    private $spec_types;
+
+    public function __construct(mysqli $mysqli, array $spec_types = null)
+    {
+        $this->mysqli = $mysqli;
+        $this->spec_types = [
+            'integer' => '?d',
+            'float' => '?f',
+            'array' => '?a',
+            'ids' => '?#'
+        ];
+        // Дополняем массив спецификаторов, при необходимости
+        if($spec_types)
+            foreach ($spec_types as $key=>$val)
+                $spec_types[$key] = $val;
+    }
+
+    private function findAllStrPos($haystack, $needle) {
+        $offset = 0;
+        $allpos = array();
+        while (($pos = strpos($haystack, $needle, $offset)) !== FALSE) {
+            $offset = $pos + 1;
+            $allpos[] = $pos;
+        }
+        return $allpos;
+    }
+
+    private function dropOptionalText($query, $spec_pos, $left_braces, $right_braces){
+        $left = $left_braces[0];
+        $right = $right_braces[count($right_braces)-1];
+        // Приближением находим ближайшие условные блоки
+        foreach ($left_braces as $lb){
+            if($lb < $spec_pos){
+                $left = $lb;
+            }
+        }
+        foreach ($right_braces as $rb){
+            if($rb > $spec_pos){
+                $right = $rb;
+            }
+        }
+        // Удаляем текст между опциональным блоком
+        return substr_replace($query, '', $left, $right);
+    }
+
+    private function castType($arg, $spec_type, $slashes = false, $apostrophe = false){
+        $type = gettype($arg);
+        if($type == 'string')
+        $arg = addslashes($arg);
+        if($type != $spec_type) {
+            // Пришёл null - кладём null
+            if($arg == null) $arg = null;
+            // Пытаемся кастовать в соответствии с нужным спец.
+            else {
+                $res = settype($arg, $spec_type);
+                if (!$res)
+                    throw new Exception("Аргумент $arg не совпадает с переданным типом $spec_type и не может быть преобразован");
+            }
+        }
+        // Если обрабатываем масив, то заменяем значения
+        if($slashes && gettype($arg) == 'string') $arg = "\'$arg\'";
+        if($slashes && gettype($arg) == 'NULL') $arg = 'NULL';
+        // Добавляем кавычки, там где нужно
+        if($apostrophe) $arg = "`$arg`";
+        return $arg;
+    }
+
+    public function buildQuery(string $query, array $args = []): string
+    {
+        $spec_count = substr_count($query, '?');
+        $args_count = count($args);
+        // Проверка на совпадение количества спец.
+        if($spec_count !== $args_count)
+            throw new Exception("Количество спецификаторов $spec_count не совпадает с количеством аргументов $args_count");
+        preg_match_all('/\?./u', $query, $matches);
+        $str_copy = $query;
+        $replacement = [];
+
+        foreach ($matches[0] as $i=>$match){
+            if($args[$i] instanceof SkipValue){
+                // Определяем текущее положение
+                $spec_pos = strpos($query, $match);
+                // Определяем все позиции
+                $left_braces = $this->findAllStrPos($query, '{');
+                $right_braces = $this->findAllStrPos($query, '}');
+
+                $query = $this->dropOptionalText($query, $spec_pos, $left_braces, $right_braces);
+
+                continue;
+            }
+            // Если передан спецификатор
+            if(in_array($match, $this->spec_types)){
+                $spec_type = array_search($match, $this->spec_types);
+                // Обрабатываем идентификаторы
+                if($spec_type == 'ids'){
+                    // Не массив -> обрабатываем как строку
+                    if(!is_array($args[$i]))
+                        array_push($replacement, $this->castType($args[$i], 'string', apostrophe: true));
+                    // Массив -> обрабатываем как обычный массив
+                    else {
+                        $arr_string = '';
+                        foreach ($args[$i] as $key=>$value) {
+                            $arr_string .= $this->castType($value, 'string', apostrophe: true);
+                            if($key != count($args[$i]) - 1) $arr_string.=', ';
+                        }
+                        array_push($replacement, $arr_string);
+                    }
+                }
+                // Обрабатываем массивы
+                elseif ($spec_type == 'array'){
+                    $arr_string = '';
+                    // Внешний итератор для корректного расставления запятых
+                    $j = 0;
+                    foreach ($args[$i] as $key=>$value){
+                        // Если массив ассоциатевный, обрабатываем также ключ
+                        if(!is_numeric($key)) {
+                            $arr_string .= $this->castType($key, gettype($key), apostrophe: true);
+                            $arr_string .= ' = ';
+                        }
+                        // Обрабатываем значение
+                        $arr_string .= $this->castType($value, gettype($value), true);
+                        if($j != count($args[$i]) - 1) $arr_string.=', ';
+                        $j++;
+                    }
+                    array_push($replacement, $arr_string);
+                }
+                // Обрабатываем остальные идентификаторы
+                else {
+                    array_push($replacement, $this->castType($args[$i], $spec_type));
+                }
+            }
+            // Если спецификатор пуст
+            elseif ($match[1] == ' '){
+                $type = gettype($args[$i]);
+                switch ($type){
+                    case 'float':
+                    case 'integer':
+                        array_push($replacement, $args[$i]);
+                        break;
+                    case 'string':
+                        array_push($replacement,  $this->castType($args[$i], 'string', true));
+                        break;
+                    case 'boolean':
+                        array_push($replacement, !!$args[$i]);
+                        break;
+                    case 'NULL':
+                        array_push($replacement, "NULL");
+                        break;
+                    default:
+                        throw new Exception("Аргумент $args[$i] имеет не поддерживаемый тип $type");
+                }
+            }
+            // Неизвестный спецификатор
+            else{
+                throw new Exception("Спецификатор $match не найден в списке");
+            }
+        }
+        $i = 0;
+        // Тут начинается замена
+        $query = preg_replace_callback('/\?./u', function ($matches) use ($replacement, &$i) {
+
+            $ret = $replacement[$i];
+            $i++;
+            // Если не уникальный спецификатор нужно добавлять доп. пробел под регулярку
+            if($matches[0][1] == ' ') $ret .= ' ';
+            return $ret;
+
+        }, $query);
+        // Убираем кавычки
+        $query = str_replace(['{', '}'], '', $query);
+        var_dump($query);
+        echo '<br>';
+//        var_dump($replacement);
+//        echo '<br>';
+//        echo '<br>';
+
+        return $query;
+//        throw new Exception();
+    }
+
+    public function skip()
+    {
+        return new SkipValue();
+    }
+}
---
+++ DatabaseTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace FpDbTest;
+
+use Exception;
+
+class DatabaseTest
+{
+    private DatabaseInterface $db;
+
+    public function __construct(DatabaseInterface $db)
+    {
+        $this->db = $db;
+    }
+
+    public function testBuildQuery(): void
+    {
+        $results = [];
+
+        $results[] = $this->db->buildQuery('SELECT name FROM users WHERE user_id = 1');
+
+        $results[] = $this->db->buildQuery(
+            'SELECT * FROM users WHERE name = ? AND block = 0',
+            ['Jack']
+        );
+
+        $results[] = $this->db->buildQuery(
+            'SELECT ?# FROM users WHERE user_id = ?d AND block = ?d',
+            [['name', 'email'], 2, true]
+        );
+
+        $results[] = $this->db->buildQuery(
+            'UPDATE users SET ?a WHERE user_id = -1',
+            [['name' => 'Jack', 'email' => null]]
+        );
+
+        foreach ([null, true] as $block) {
+            $results[] = $this->db->buildQuery(
+                'SELECT name FROM users WHERE ?# IN (?a){ AND block = ?d}',
+                ['user_id', [1, 2, 3], $block ?? $this->db->skip()]
+            );
+        }
+
+        // Добавил пару тестов, чтоб "наверняка"
+        $results[] = $this->db->buildQuery(
+            'SELECT * FROM users WHERE test = ? AND name = ? AND x1 = ? AND block = 0',
+            ['Jack', 34, null]
+        );
+        // В этом примере "удаляется" только один условный блок
+        $results[] = $this->db->buildQuery(
+            'SELECT name FROM users WHERE ?# IN (?a){ AND block = ?d}{ AND test = ?}',
+            ['user_id', [1, 2, 3], true, $this->db->skip()]
+        );
+
+        // В изначальной версии строки в массиве были в одинарных кавычках,
+        // из-за этого, обратный слеш игнорировался и сравнение происходило
+        // некорректно в экранированных строках
+        $correct = [
+            "SELECT name FROM users WHERE user_id = 1",
+            "SELECT * FROM users WHERE name = \'Jack\' AND block = 0",
+            "SELECT `name`, `email` FROM users WHERE user_id = 2 AND block = 1",
+            "UPDATE users SET `name` = \'Jack\', `email` = NULL WHERE user_id = -1",
+            "SELECT name FROM users WHERE `user_id` IN (1, 2, 3)",
+            "SELECT name FROM users WHERE `user_id` IN (1, 2, 3) AND block = 1",
+            "SELECT * FROM users WHERE test = \'Jack\' AND name = 34 AND x1 = NULL AND block = 0",
+            "SELECT name FROM users WHERE `user_id` IN (1, 2, 3) AND block = 1",
+        ];
+        // Для проверок, в случае ошибки в тесте
+//        $k = 1;
+//        foreach (str_split($correct[$k]) as $i=>$char) {
+//                if($correct[$k][$i] != $results[$k][$i]){
+//                    var_dump($correct[$k][$i], $results[$k][$i], $results[$k], $correct[$k]);
+//                    return;
+//                }
+//        }
+        if ($results !== $correct) {
+            throw new Exception('Failure.');
+        }
+    }
+}
